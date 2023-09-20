62e8aa776d2db93ebbfaeff372e02f68506f9071
Абдула Дукузов <abduladukuzov@yandex.ru>
2023-09-20 14:42:28 +0300

    3

---
+++ migrations/versions/ef60eb3004d1_initial_migration.py
@@ -0,0 +1,24 @@
+"""Initial migration
+
+Revision ID: ef60eb3004d1
+Revises: 
+Create Date: 2023-09-20 14:41:26.111228
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'ef60eb3004d1'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
